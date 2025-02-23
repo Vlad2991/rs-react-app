f980709b6790da633d66f4fb842bc07efeb99274
Vlad2991 <vlad29911710@gmail.com>
2025-02-23 18:17:45 +0300

    Init: start-finsh

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from 'react-redux';
+import { store } from './store';
+import { ThemeProvider } from './ThemeContext';
 import SearchBar from "./components/SearchBar";
 import Results from "./components/Results";
 import ErrorBoundary from "./components/ErrorBoundary";
-import NotFound from "./components/NotFound"; // Импортируем компонент NotFound
+import NotFound from "./components/NotFound";
 import "./App.css";
 
 const App: React.FC = () => {
@@ -14,24 +17,27 @@ const App: React.FC = () => {
   };
 
   return (
-    <ErrorBoundary>
-      <Router>
-        <div className="app">
-          <h1>Поиск покемонов</h1>
-          <SearchBar onSearch={handleSearch} />
-          <Routes>
-            <Route path="/" element={<Results query={query} />} />
-            <Route path="/search/:page" element={<Results query={query} />} />
-            <Route
-              path="/search/:page/details/:details"
-              element={<Results query={query} />}
-            />
-            {/* Добавляем маршрут для страницы 404 */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
-      </Router>
-    </ErrorBoundary>
+    <Provider store={store}>
+      <ThemeProvider>
+        <ErrorBoundary>
+          <Router>
+            <div className="app">
+              <h1>Поиск покемонов</h1>
+              <SearchBar onSearch={handleSearch} />
+              <Routes>
+                <Route path="/" element={<Results query={query} />} />
+                <Route path="/search/:page" element={<Results query={query} />} />
+                <Route
+                  path="/search/:page/details/:details"
+                  element={<Results query={query} />}
+                />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </div>
+          </Router>
+        </ErrorBoundary>
+      </ThemeProvider>
+    </Provider>
   );
 };
 
